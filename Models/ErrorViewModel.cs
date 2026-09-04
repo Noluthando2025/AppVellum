cbc86db6ecc6e77ed95aa6f83c9d29f226bc750d
NTOKOZO <ST10455454@rcconnect.edu.za>
2026-09-04 20:55:53 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppVellum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
